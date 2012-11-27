d19ae369e482ca7deb8bacf5bd3d9fb471cf3d6e
Solomon Hykes <solomon@dotcloud.com>
2012-11-27 15:26:31 -0800

    Added HTTP handler example

---
+++ examples/webapp.go
@@ -0,0 +1,26 @@
+
+/*
+** webapp.go ADDR: serve a standard http handler over spdy
+**
+** Usage:
+**
+**      go run webapp.go :4242
+**
+**      spdycat :4242 :path=/
+*/
+
+package main
+
+import (
+    "net/http"
+    "github.com/shykes/spdy.go"
+    "flag"
+)
+
+/* Replace this with anything which implements http.Handler */
+var webapp http.Handler = http.FileServer(http.Dir("."))
+
+func main() {
+    flag.Parse()
+    spdy.ServeTCP(flag.Args()[0], &spdy.HTTPHandler{webapp})
+}
