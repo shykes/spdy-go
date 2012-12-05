shykes/spdy-go

commit ad2b4ecd196e7782c57cb36ee81cab613844931e

StreamReader.Push() and StreamReader.Receive() no longer pass headers, for simplicity and symmetry with StreamWriter.Send(). TODO: find a better API for exposing new headers

Stream.go
@@ -13,7 +13,7 @@ import (
 
 
 type Receiver interface {
-    Receive() (*[]byte, *http.Header, error)
+    Receive() (*[]byte, error)
 }
 
 type Sender interface {
@@ -62,15 +62,10 @@ type StreamReader struct {
     readBuffer  bytes.Buffer
 }
 
-type streamMessage struct {
-    data    *[]byte
-    headers *http.Header
-}
-
 func (r *StreamReader) Read(data []byte) (int, error) {
     debug("Read(max %d)\n", len(data))
     for r.readBuffer.Len() == 0 {
-        msg, _, err := r.Receive()
+        msg, err := r.Receive()
         if err != nil {
             return 0, err
         }
@@ -86,7 +81,7 @@ func (r *StreamReader) Read(data []byte) (int, error) {
 func (reader *StreamReader) WriteTo(dst io.Writer) (int64, error) {
     var written int64 = 0
     for {
-        data, _, err := reader.Receive()
+        data, err := reader.Receive()
         if err != nil && err != io.EOF {
             return written, err
         }
@@ -109,18 +104,15 @@ func (reader *StreamReader) Headers() *http.Header {
     return &reader.headers
 }
 
-func (reader *StreamReader) Receive() (*[]byte, *http.Header, error) {
+func (reader *StreamReader) Receive() (*[]byte, error) {
     msg, err := reader.data.Receive()
-    if msg == nil {
-        return nil, nil, err
-    }
-    return msg.(*streamMessage).data, msg.(*streamMessage).headers, err
+    return msg.(*[]byte), err
 }
 
 /* Receive and discard all incoming messages until `headerName` is set, then return its value */
 func (reader *StreamReader) WaitForHeader(key string) (string, error) {
     for reader.Headers().Get(key) == "" {
-        _, _, err := reader.Receive()
+        _, err := reader.Receive()
         if err != nil {
             return "", err
         }
@@ -129,12 +121,8 @@ func (reader *StreamReader) WaitForHeader(key string) (string, error) {
 }
 
 
-func (reader *StreamReader) Push(data *[]byte, headers *http.Header) {
-    err := reader.data.Send(&streamMessage{data, headers})
-    if err != nil {
-        // FIXME: should we trigger a protocol error here? or in Session?
-        debug("%s\n", err)
-    }
+func (reader *StreamReader) Push(data *[]byte) error {
+    return reader.data.Send(data)
 }
 
 func (reader *StreamReader) Error(err error) {
@@ -149,36 +137,6 @@ func (reader *StreamReader) Close() {
 
 func (reader *StreamReader) Closed() bool {
     return reader.data.Closed()
-}
-
-
-/* Send all output from a reader to another writer */
-
-func (reader *StreamReader) Pipe(writer *StreamWriter) (error, error) {
-    for {
-        data, headers, err := reader.Receive()
-        if (err != nil) && (err != io.EOF) {
-            return err, nil
-        }
-        eof := err == io.EOF
-        if headers != nil { // FIXME: can data and headers both be non-nil?
-            updateHeaders(writer.Headers(), headers)
-            err := writer.SendHeaders(false)
-            if err != nil {
-                return nil, err
-            }
-        } else if data != nil {
-            err := writer.Send(data)
-            if err != nil {
-                return nil, err
-            }
-        }
-        if eof {
-            writer.Close()
-            return nil, nil
-        }
-    }
-    return nil, nil
 }
 
 

Session.go
@@ -187,7 +187,7 @@ func (session *Session) receiveLoop() error {
                 debug("Received a data frame for unknown stream id %s. Dropping.\n", dframe.StreamId)
                 continue
             }
-            stream.Input.Push(&dframe.Data, nil)
+            stream.Input.Push(&dframe.Data)
             if dframe.Flags & spdy.DataFlagFin != 0 {
                 stream.Input.Close()
             }
@@ -199,7 +199,8 @@ func (session *Session) receiveLoop() error {
                 debug("Received headers for unknown stream id %s. Dropping.\n", headersframe.StreamId)
                 continue
             }
-            stream.Input.Push(nil, &headersframe.Headers)
+            updateHeaders(stream.Input.Headers(), &headersframe.Headers)
+            // FIXME: notify of new headers?
             if headersframe.CFHeader.Flags & spdy.ControlFlagFin != 0 {
                 stream.Input.Close()
             }
@@ -238,7 +239,6 @@ func (session *Session) receiveLoop() error {
             }
             /* Set the initial headers */
             updateHeaders(stream.Input.Headers(), &synReplyFrame.Headers)
-            stream.Input.Push(nil, &synReplyFrame.Headers)
             /* If FLAG_FIN is set, half-close the stream */
             if synReplyFrame.CFHeader.Flags & spdy.ControlFlagFin != 0 {
                 stream.Input.Close()

commit 2859fd81f008392b9abe2450f6ac896595eaab3b

Fixed crash at disconnect introduced in ad2b4ecd196e7782c57cb36ee81cab613844931e

Stream.go
@@ -105,7 +105,10 @@ func (reader *StreamReader) Headers() *http.Header {
 
 func (reader *StreamReader) Receive() (*[]byte, error) {
     msg, err := reader.MQ.Receive()
-    return msg.(*[]byte), err
+    if err != nil {
+        return nil, err
+    }
+    return msg.(*[]byte), nil
 }
 
 /* Receive and discard all incoming messages until `headerName` is set, then return its value */