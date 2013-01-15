63c0b660de70f4940fd2b44a3d125ea0dfbbf3fb
Solomon Hykes <solomon@dotcloud.com>
2013-01-14 19:58:55 -0800

    Very simple webapp example

---
+++ examples/webapp.go
@@ -0,0 +1,36 @@
+
+/*
+** webapp.go ADDR: serve a standard http handler over spdy
+**
+** Usage:
+**
+**      go run webapp.go :4242
+**
+**      spdycat :4242 :path=/
+*/
+
+package main
+
+import (
+    "net/http"
+    "github.com/shykes/spdy-go"
+    "flag"
+    "log"
+)
+
+/* Replace this with anything which implements http.Handler */
+var webapp http.Handler = http.FileServer(http.Dir("."))
+
+func main() {
+    tls := flag.Bool("t", false, "Enable TLS")
+    flag.Parse()
+    var err error
+    if *tls {
+	    err = spdy.ListenAndServeTLS(flag.Args()[0], "cert.pem", "key.pem", webapp)
+    } else {
+	    err = spdy.ListenAndServeTCP(flag.Args()[0], webapp)
+    }
+    if err != nil {
+	log.Fatal(err)
+    }
+}
