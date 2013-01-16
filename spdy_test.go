c4e5ebeeaf39d399934a4f61726298fc769cada2
Solomon Hykes <solomon@dotcloud.com>
2013-01-15 17:56:07 -0800

    Consolidated all tests in spdy_test.go

--- spdy_test.go
+++ spdy_test.go
@@ -13,6 +13,9 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
+	"errors"
+	"fmt"
+	"sync"
 )
 
 func TestHeaderParsing(t *testing.T) {
@@ -589,5 +592,476 @@ func checkZeroStreamId(t *testing.T, frame string, method string, err error) {
 	eerr, ok := err.(*Error)
 	if !ok || eerr.Err != ZeroStreamId {
 		t.Errorf("%s ZeroStreamId, incorrect error %#v, frame %s", method, eerr, frame)
+	}
+}
+
+// [...] If the server is initiating the stream, the Stream-ID must be even. [...]
+
+func TestServerStreamIdMustBeEven(t *testing.T) {
+    s := NewSession(new(DummyHandler), true)
+	for i:=0; i<42; i+=1 {
+	    stream, err := s.InitiateStream()
+	 if err != nil {
+	    t.Error(err)
+	}
+	if stream.Id % 2 != 0 {
+	    t.Errorf("If the server is initiating the stream, the Stream-ID must be even.")
+	}
+    }
+}
+
+// [...] If the client is initiating the stream, the Stream-ID must be odd. [...]
+
+func TestClientStreamIdMustBeOdd(t *testing.T) {
+    s := NewSession(new(DummyHandler), false)
+	for i:=0; i<42; i+=1 {
+	    stream, err := s.InitiateStream()
+	 if err != nil {
+	    t.Error(err)
+	}
+	if stream.Id % 2 != 1 {
+	    t.Errorf("If the client is initiating the stream, the Stream-ID must be odd.")
+	}
+    }
+}
+
+// [...] 0 is not a valid Stream-ID. [...]
+
+func TestStreamIDZeroNotValid(t *testing.T) {
+    for _, isServer := range []bool{true, false} {
+	s := NewSession(new (DummyHandler), isServer)
+	    stream, err := s.InitiateStream()
+	    if err != nil {
+		t.Error(err)
+	    }
+	    if stream.Id == 0 {
+		t.Errorf("0 is not a valid Stream-ID.")
+	    }
+	    if err := s.WriteFrame(&SynStreamFrame{StreamId: 0}); err != nil {
+		t.Error(err)
+	    }
+	    // Write a ping frame so there's always at least one frame
+	    // to read
+	    if err := s.WriteFrame(&PingFrame{}); err != nil {
+		t.Error(err)
+	    }
+	    frame, err := s.ReadFrame()
+	    if rstFrame, isRst := frame.(*RstStreamFrame); !isRst {
+		t.Errorf("0 is not a valid Stream-ID.")
+	    } else {
+		if rstFrame.Status != ProtocolError {
+		    t.Errorf("0 is not a valid Stream-ID.")
+		}
+		if rstFrame.StreamId != 0 {
+		    t.Errorf("0 is not a valid Stream-ID.")
+		}
+	    }
+	}
+}
+
+// [...] Stream-IDs from each side of the connection must increase monotonically as new
+// streams are created [...]
+
+func TestStreamIDIncrementLocal(t *testing.T) {
+	for _, isServer := range []bool{true, false} {
+		s := NewSession(new(DummyHandler), isServer)
+		var previousId uint32
+		for i:=0; i<42; i+=1 {
+			stream, err := s.InitiateStream()
+			if err != nil {
+				t.Error(err)
+			}
+			if previousId != 0 && stream.Id != previousId + 2 {
+				t.Error("Stream-IDs from each side of the connection must increase monotically as new streams are created")
+			}
+		}
+	}
+}
+
+// [...] E.g. Stream 2 may be created after stream 3, [...]
+
+func TestStream2AfterStream3(t *testing.T) {
+	s := NewSession(new(DummyHandler), false)
+	s.InitiateStream()
+	stream3, err := s.InitiateStream(); if err != nil {
+		t.Error(err)
+	} else {
+		if stream3.Id != 3 {
+			t.Error("Second client-initiated stream should have ID=3")
+		}
+	}
+	if _, err := SendExpect(s, &SynStreamFrame{StreamId: 2}, nil); err != nil {
+		t.Error(err)
+	}
+}	
+
+//    but stream 7 must not be created after stream 9.
+
+func TestStream7AfterStream9(t *testing.T) {
+	s := NewSession(new(DummyHandler), true)
+	if _, err := SendExpect(s, &SynStreamFrame{StreamId:9}, nil); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := SendExpect(s, &SynStreamFrame{StreamId:7}, reflect.TypeOf(&RstStreamFrame{})); err != nil {
+		t.Fatal(err)
+	}
+}	
+
+//    Stream IDs do not
+//    wrap: when a client or server cannot create a new stream id without
+//    exceeding a 31 bit value, it MUST NOT create a new stream.
+
+func TestIdWrap(t *testing.T) {
+	for _, isServer := range []bool{true, false} {
+		s := NewSession(new(DummyHandler), isServer)
+		if isServer {
+			s.lastStreamIdOut = 0xffffffff
+		} else {
+			s.lastStreamIdOut = 0xffffffff - 1
+		}
+		_, err := s.InitiateStream()
+		if err == nil {
+			t.Error("[...] when a client or server cannot create a new stream id without exceeding a 31 bit value, it MUST NOT create a new stream")
+		}
+		if s.NStreams() != 0 {
+			t.Error("[...] when a client or server cannot create a new stream id without exceeding a 31 bit value, it MUST NOT create a new stream")
+
+		}
+	}
+}
+
+func SendExpect(s *Session, frameIn Frame, frameTypeOut reflect.Type) (Frame, error) {
+	if err := s.WriteFrame(&PingFrame{Id:42}); err != nil {
+		return nil, err
+	}
+	if err := s.WriteFrame(frameIn); err != nil {
+		return nil, err
+	}
+	frame, err := s.ReadFrame()
+	if err != nil {
+		return nil, err
+	}
+	var receivedNothing bool
+	switch ff := frame.(type) {
+		case *PingFrame: {
+			if ff.Id == 42 {
+				receivedNothing = true
+			}
+		}
+	}
+	if receivedNothing {
+		if frameTypeOut != nil {
+			// Expected something but received nothing", frameTypeOut)
+		}
+	} else {
+		if frameTypeOut == nil {
+			// Expected nothing but received something
+			return nil, errors.New(fmt.Sprintf("Expected nothing but received %#v", frame))
+		} else if frameTypeOut != reflect.TypeOf(frame) {
+			return nil, errors.New("Received wrong frame type")
+		}
+	}
+	if frameTypeOut != nil {
+		if _, err := s.ReadFrame(); err != nil {
+			return nil, err
+		}
+	}
+	return frame, nil
+}
+
+func TestSynStreamInvalidId(t *testing.T) {
+	s := NewTestSession(nil, true)
+	if err := s.pipe.WriteFrame(&SynStreamFrame{StreamId: 2}); err != nil {
+		t.Error(err)
+	}
+	frame, err := s.pipe.ReadFrame()
+	if err != nil {
+		t.Error("Session didn't send protocol error on invalid stream id")
+		return
+	}
+	if f, isRst := frame.(*RstStreamFrame); isRst {
+		if f.Status != ProtocolError {
+			t.Error("Session didn't send protocol error on invalid stream id")
+		}
+	} else {
+		t.Error("Session didn't send protocol error on invalid stream id")
+	}
+
+}
+
+
+func TestSessionInitiateStreamServer(t *testing.T) {
+	session := NewTestSession(nil, true)
+	stream, err := session.InitiateStream()
+	if err != nil {
+		t.Error(err)
+	}
+	if stream.Id != 2 {
+		t.Errorf("First server-created stream should be 2 (not %d)", stream.Id)
+	}
+}
+
+func TestSessionInitiateStreamClient(t *testing.T) {
+	session := NewTestSession(nil, false)
+	stream, err := session.InitiateStream()
+	if err != nil {
+		t.Error(err)
+	}
+	if stream.Id != 1 {
+		t.Errorf("First client-created stream should be 1 (not %d)", stream.Id)
+	}
+}
+
+
+func TestNStreams(t *testing.T) {
+	session := NewTestSession(nil, true)
+	if session.NStreams() != 0 {
+		t.Errorf("NStreams() for empty session should be 0 (not %d)", session.NStreams())
+	}
+	session.InitiateStream()
+	if session.NStreams() != 1 {
+		t.Errorf("NStreams() should be 1 (not %d)", session.NStreams())
+	}
+}
+
+func TestCloseStream(t *testing.T) {
+	session := NewTestSession(nil, false)
+	session.InitiateStream()
+	session.CloseStream(1)
+	if session.NStreams() != 0 {
+		t.Errorf("CloseStream() did not delete the stream")
+	}
+}
+
+func TestHeadersCrash(t *testing.T) {
+	_, peer := NewStream(1, false)
+	headers := http.Header{}
+	headers.Add("foo", "bar")
+	err := peer.WriteFrame(&SynStreamFrame{
+		StreamId:	1,
+		Headers:	headers,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if peer.output.Headers.Get("foo") != "bar" {
+		t.Errorf("Stream input didn't store headers (%v != %v)", peer.output.Headers, headers)
+	}
+}
+
+func TestBodyWrite(t *testing.T) {
+	data := []byte("hello world\n")
+	session := NewSession(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(data)
+	}), true)
+	session.WriteFrame(&SynStreamFrame{StreamId: 1})
+	session.ReadFrame()
+	frame, err := session.ReadFrame()
+	if err != nil {
+		t.Error(err)
+	}
+	if dataframe, isData := frame.(*DataFrame); !isData {
+		t.Error("Writing to ResponseWriter body failed")
+	} else {
+		if !bytes.Equal(dataframe.Data, data) {
+			t.Error("ResponseWriter sent |%#v| instead of |%#v|", dataframe.Data, data)
+		}
+	}
+}
+
+func TestBodyRead(t *testing.T) {
+	data := []byte("hello world\n")
+	var locker sync.Mutex
+	locker.Lock()
+	session := NewSession(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		result := make([]byte, len(data))
+		n, err := r.Body.Read(result)
+		if err != nil {
+			t.Error(err)
+		}
+		if n != len(data) {
+			t.Error("Request body received %d bytes instead of %d", n, len(data))
+		}
+		locker.Unlock()
+	}), true)
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1}); err != nil {
+		t.Error(err)
+	}
+	if err := session.WriteFrame(&DataFrame{StreamId: 1, Data: data}); err != nil {
+		t.Error(err)
+	}
+	locker.Lock()
+}
+
+func TestBodyReadClose(t *testing.T) {
+	data := []byte("hello world\n")
+	var locker sync.Mutex
+	locker.Lock()
+	session := NewSession(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		result := make([]byte, len(data))
+		n, err := r.Body.Read(result)
+		if err != nil {
+			t.Error(err)
+		}
+		if n != len(data) {
+			t.Error("Request body received %d bytes instead of %d", n, len(data))
+		}
+		n, err = r.Body.Read(result)
+		if err != io.EOF {
+			t.Error("Request body was not closed")
+		}
+		if n != 0 {
+			t.Error("Body.Read() should have returned 0, returned %d instead", n)
+		}
+		locker.Unlock()
+	}), true)
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1}); err != nil {
+		t.Error(err)
+	}
+	if err := session.WriteFrame(&DataFrame{StreamId: 1, Data: data, Flags: DataFlagFin}); err != nil {
+		t.Error(err)
+	}
+	locker.Lock()
+}
+
+func TestHeadersRead(t *testing.T) {
+	var locker sync.Mutex
+	locker.Lock()
+	session := NewSession(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("foo") != "bar" {
+			t.Errorf("Request header 'foo' shouldbe 'bar', but it's '%s'", r.Header.Get("foo"))
+		}
+		locker.Unlock()
+	}), true)
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1, Headers: http.Header{"foo": {"bar"}}}); err != nil {
+		t.Error(err)
+	}
+	locker.Lock()
+}
+
+func TestHeadersWrite(t *testing.T) {
+	session := NewSession(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("foo", "bar")
+		w.WriteHeader(200)
+	}), true)
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1}); err != nil {
+		t.Error(err)
+	}
+	if frame, err := session.ReadFrame(); err != nil {
+		t.Error(err)
+	} else {
+		if replyFrame, isReply := frame.(*SynReplyFrame); !isReply {
+			t.Errorf("HTTPResponse.WriteHeader() did not send a SYN_REPLY frame (%#v)", frame)
+		} else {
+			if replyFrame.Headers.Get("foo") != "bar" {
+				t.Errorf("Header foo should be bar, but it's %s", replyFrame.Headers.Get("foo"))
+			}
+		}
+	}
+}
+
+func TestHeadersWriteDefault(t *testing.T) {
+	session := NewSession(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	}), true)
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1}); err != nil {
+		t.Error(err)
+	}
+	if frame, err := session.ReadFrame(); err != nil {
+		t.Error(err)
+	} else {
+		if replyFrame, isReply := frame.(*SynReplyFrame); !isReply {
+			t.Errorf("HTTPResponse.WriteHeader() did not send a SYN_REPLY frame (%#v)", frame)
+		} else {
+			if replyFrame.Headers.Get("status") != "200" {
+				t.Errorf("Header status should be 200, but it's %s", replyFrame.Headers.Get("status"))
+			}
+		}
+	}
+}
+
+func TestURL(t *testing.T) {
+	var locker sync.Mutex
+	locker.Lock()
+	session := NewSession(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/foo/bar" {
+			t.Errorf("Request URL should be '/foo/bar', but it is '%s'", r.URL.Path)
+		}
+		locker.Unlock()
+	}), true)
+	headers := make(http.Header)
+	headers.Set("url", "/foo/bar")
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1, Headers: headers}); err != nil {
+		t.Error(err)
+	}
+	locker.Lock()
+}
+
+func TestMethod(t *testing.T) {
+	var locker sync.Mutex
+	locker.Lock()
+	session := NewSession(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			t.Errorf("Request method should be 'POST', but it is '%s'", r.Method)
+		}
+		locker.Unlock()
+	}), true)
+	headers := make(http.Header)
+	headers.Set("method", "POST")
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1, Headers: headers}); err != nil {
+		t.Error(err)
+	}
+	locker.Lock()
+}
+
+func TestDefaultMethod(t *testing.T) {
+	var locker sync.Mutex
+	locker.Lock()
+	session := NewSession(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			t.Errorf("Request method should be 'GET', but it is '%s'", r.Method)
+		}
+		locker.Unlock()
+	}), true)
+	if err := session.WriteFrame(&SynStreamFrame{StreamId: 1}); err != nil {
+		t.Error(err)
+	}
+	locker.Lock()
+}
+
+func TestSendOneFrame(t *testing.T) {
+	pipeR, pipeW := Pipe(1)
+	f_in := NoopFrame{}
+	pipeW.WriteFrame(&f_in)
+	f_out, _ := pipeR.ReadFrame()
+	if f_in != *f_out.(*NoopFrame) {
+		t.Errorf("Sent %v, received %v\n", f_in, f_out)
+	}
+}
+
+func TestCloseWriter(t *testing.T) {
+	r, w := Pipe(1)
+	frame_in := &NoopFrame{}
+	w.WriteFrame(frame_in)
+	w.Close()
+	if frame_out, err := r.ReadFrame(); err != nil {
+		t.Error(err)
+	} else if frame_in != frame_out.(*NoopFrame) {
+		t.Errorf("Received wrong frame after closing (%#v)", frame_out)
+	}
+	if frame_out, err := r.ReadFrame(); err != io.EOF || frame_out != nil {
+		t.Errorf("ReadFrame() on a closed pipe returned (%#v, %#v) instead of (nil, EOF)", frame_out, err)
+	}
+}
+
+func TestCloseReader(t *testing.T) {
+	r, w := Pipe(1)
+	frame_in := &NoopFrame{}
+	r.Close()
+	if err := w.WriteFrame(frame_in); err != io.ErrClosedPipe {
+		t.Errorf("WriteFrame() on a closed pipe returned %#v instead of ErrClosedPipe", err)
+	}
+	if frame, err := r.ReadFrame(); err != io.ErrClosedPipe || frame != nil  {
+		t.Errorf("ReadFrame() on a closed pipe reader returned (%#v, %#v) instead of (nil, ErrClosedPipe)", frame, err)
 	}
 }
